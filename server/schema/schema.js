1143e2c3a2f48b39d4416e80f5853afd0f143019
Dotun Ogunsakin <email@dotun.me>
2018-08-29 01:56:51 -0400

    Implemented working schema (with dummy book data).

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,16 +1,56 @@
 const graphql = require("graphql");
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema} = graphql;
 
-const Book = new GraphQLObjectType({
+const books = [
+    {id: 1, name: "Tippo and Friends", genre: "Children's Fiction"},
+    {id: 2, name: "1984", genre: "Adult Fiction"},
+    {id: 3, name: "The C programming Language", genre: "Educational"},
+];
+
+const BookType = new GraphQLObjectType({
     name: "Book",
-    fields: () => {
-        id: {type: GraphQLString},
+    fields: () => {return {
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString},
-    },
+    }},
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: "Author",
+    fields: () => {return {
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+    }},
 });
 
 const RootQuery = new GraphQLObjectType({
     name: "RootQuery",
-    fields: 
+    fields: {
+        book: {
+            type: BookType,
+            args: {
+                id: {type: GraphQLID},
+            },
+            resolve: function(parent, args) {
+                return (books.filter(
+                                (book) => {return book.id === Number(args.id);})[0]
+                        );
+            },
+        },
+        author: {
+            type: AuthorType,
+            args: {
+                id: {type: GraphQLID},
+                name: {type: GraphQLString},
+            },
+            resolve: function(parent, args) {
+
+            },
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
 })
